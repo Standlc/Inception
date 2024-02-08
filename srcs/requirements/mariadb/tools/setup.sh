9373d17f2a82df2ebbd4ed722564898c32b3ceb1
“standlc” <s.delacomble@gmail.com>
2024-02-08 09:40:24 +0100

    loop unitl mariadb starts

--- srcs/requirements/mariadb/tools/setup.sh
+++ srcs/requirements/mariadb/tools/setup.sh
@@ -1,11 +1,12 @@
 mysqld_safe &
 
+# wait for server to start
 while ! mysql -u root -e ";" 2> /dev/null; do
 	sleep 1
 done
 
 # creating the wordpress DB and user
-mysql -e "CREATE DATABASE $DB_NAME;"
+mysql -e "CREATE DATABASE IF NOT EXISTS $DB_NAME;"
 mysql -e "CREATE USER IF NOT EXISTS $DB_USER@localhost IDENTIFIED BY '$DB_PASSWORD';"
 mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO $DB_USER@'%' IDENTIFIED BY '$DB_PASSWORD';"
 mysql -e "FLUSH PRIVILEGES;"
--- srcs/requirements/wordpress/tools/setup.sh
+++ srcs/requirements/wordpress/tools/setup.sh
@@ -1,9 +1,9 @@
 cd /wordpress
 
-# while ! mysql -h mariadb -u $DB_USER -p $DB_PASSWORD $DB_NAME -e ";" 2>/dev/null ; do
-# 		echo "Waiting for mariadb container to start."
-		sleep 5
-# done
+# wait for mariadb to start
+while ! mariadb -h mariadb -u $DB_USER -p$DB_PASSWORD $DB_NAME -e ";" 2>/dev/null ; do
+	sleep 1
+done
 
 if ! test -f wp-config.php; then
     wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASSWORD --dbhost=mariadb:3306
